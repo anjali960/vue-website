52a65c3c69df26b223c9dce64e03c59c485c5f2b
anjali960 <anjalibaghel960@gmail.com>
2023-07-17 17:12:25 +0530

    includes sass

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,28 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app white>
+    <div><Header /></div>
+
+    <v-main>
+      <router-view />
+    </v-main>
+
+    <v-footer> <Footer /></v-footer>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
 
-#nav {
-  padding: 30px;
+export default {
+  name: "App",
+  components: {
+    Header,
+    Footer,
+  },
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+  data: () => ({
+    //
+  }),
+};
+</script>
